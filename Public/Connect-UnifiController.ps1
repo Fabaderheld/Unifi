c85dd0201e81fc5774a0ffef17c638a9f7c0895a
Fabian Sasse <fabaderheld@gmail.com>
2024-05-24 11:27:39 +0200

    Fixed Certificate waring for PS7

--- Public/Connect-UnifiController.ps1
+++ Public/Connect-UnifiController.ps1
@@ -7,36 +7,36 @@ function Connect-UnifiController {
         [Parameter(ParameterSetName = 'Connect')][switch]$UDMPro
     )
 
-    if (!$refresh){
+    if (!$refresh) {
 
         if ($UDMPro) {
-            $script:LoginURI="$ControllerURL/api/auth/login"
-            $ControllerURL = $ControllerURL+"/proxy/network"
+            $script:LoginURI = "$ControllerURL/api/auth/login"
+            $ControllerURL = $ControllerURL + "/proxy/network"
         }
         else {
-            $script:LoginURI="$ControllerURL/api/login"
+            $script:LoginURI = "$ControllerURL/api/login"
         }
         $script:Controller = $ControllerURL
         $script:Credentials = $credentials
     }
-    
+
     $params = @{
         username = $script:credentials.GetNetworkCredential().UserName
         password = $script:credentials.GetNetworkCredential().password
     }
     $body = New-UnifiCommand $params
-    
+
     try {
-        $results = Invoke-Restmethod -Uri $LoginURI -method post -body $body -ContentType "application/json; charset=utf-8"  -SessionVariable myWebSession
+        $results = Invoke-RestMethod -Uri $LoginURI -Method post -Body $body -ContentType "application/json; charset=utf-8"  -SessionVariable myWebSession -SkipCertificateCheck
         if ($results.meta.rc -eq "ok") {
-            if (!$refresh){
+            if (!$refresh) {
                 Write-Verbose "Successfully connected to Unifi controller."
             }
             else {
                 Write-Verbose "Successfully reconnected to Unifi controller."
             }
-        # Set this as a script variable as it will be used for all other commands in the module.
-        $Script:Session = $MyWebSession
+            # Set this as a script variable as it will be used for all other commands in the module.
+            $Script:Session = $MyWebSession
         }
     }
     catch {
@@ -48,8 +48,8 @@ function Connect-UnifiController {
         Connects to the Unifi controller.
 
         .DESCRIPTION
-        Connects to the Unifi controller. If you are connecting to a UDMPro, make sure to use the '-UDMPro' switch. The connection 
-        to the Unifi controller will automatically be renewed when the current session expires. 
+        Connects to the Unifi controller. If you are connecting to a UDMPro, make sure to use the '-UDMPro' switch. The connection
+        to the Unifi controller will automatically be renewed when the current session expires.
 
         .PARAMETER ControllerURL
         Complete URI for the controller. i.e.: https://mycontroller:8443
@@ -62,7 +62,7 @@ function Connect-UnifiController {
 
         .NOTES
         Notes and code snippets for building required variables to use with this command:
-        
+
         [String]$Hostname = '<FQDN or IP address of the controller>'
         [String]$Port = '8443' # Change this to match the listening port
         [String]$UnifiUsername = '<Unifi controller username>'
@@ -73,7 +73,7 @@ function Connect-UnifiController {
         # Enables TLS1.2 -- this is a universal method that works for any dot net version
         [Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([Net.SecurityProtocolType], 3072)
 
-        # Ignore self-signed certificates 
+        # Ignore self-signed certificates
         [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
 
         # Create a secure credential object
@@ -88,11 +88,11 @@ function Connect-UnifiController {
         None.
 
         .OUTPUTS
-        None.    
+        None.
 
         .EXAMPLE
         Connect-UnifiController -ControllerURL $Controller -credentials $Credentials
-        
+
         Connects to the Unifi controller URI specified in the $controller variable and uses the credentials provided in the $Credentials variable.
         $Controller is the full URI and port required for accessing the controller's web UI. The $Credentials variable is a PSCredential object.
         Please see notes for more information about how to create these variables.
--- Public/Disconnect-UnifiController.ps1
+++ Public/Disconnect-UnifiController.ps1
@@ -1,22 +1,22 @@
 function Disconnect-UnifiController {
     [CmdletBinding()]
     # Disconnect from Unifi controller
-    $results = Invoke-Restmethod -Uri "$controller/api/logout" -method post -ContentType "application/json; charset=utf-8" -Websession $session
+    $results = Invoke-RestMethod -Uri "$controller/api/logout" -Method post -ContentType "application/json; charset=utf-8" -WebSession $session -SkipCertificateCheck
     if ($results.meta.rc -eq "ok") {
-        write-host "Successfully disconnected from Unifi controller."
+        Write-Host "Successfully disconnected from Unifi controller."
     }
     <#
         .SYNOPSIS
         Disconnects from the Unifi controller.
 
         .DESCRIPTION
-        Disconnects from the Unifi controller. This will invalidate the current session. This command really isn't needed, 
+        Disconnects from the Unifi controller. This will invalidate the current session. This command really isn't needed,
         but is included for completeness. The API supports a session logout, so this functionality is present in the module.
 
         .INPUTS
         None.
 
         .OUTPUTS
-        None.    
+        None.
     #>
 }
